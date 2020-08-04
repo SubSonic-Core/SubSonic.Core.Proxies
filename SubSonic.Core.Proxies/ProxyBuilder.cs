74478f2219dff058a1bf25fec89baaf30081e99f
Kenneth Carter <kccarter32@gmail.com>
2020-08-04 11:47:43 -0600

    added proxy builder

---
+++ SubSonic.Core.Proxies/ProxyBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace SubSonic.Core.Proxies
+{
+    internal sealed class ProxyBuilder
+    {
+        public string ProxyName { get; set; }
+        public Type TypeOfProxy { get; set; }
+        public Type CtorType { get; set; }
+        public AssemblyBuilder AssemblyBuilder { get; set; }
+        public ModuleBuilder ModuleBuilder { get; set; }
+        public TypeBuilder TypeBuilder { get; set; }
+    }
+}
