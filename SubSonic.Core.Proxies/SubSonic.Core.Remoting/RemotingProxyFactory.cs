4f10936af06d6d2922ad794b97b2461ddbd86d27
Kenneth Carter <kccarter32@gmail.com>
2020-08-09 14:48:30 -0600

    add stub class for RemotingProxyFactory

---
+++ SubSonic.Core.Proxies/SubSonic.Core.Remoting/RemotingProxyFactory.cs
@@ -0,0 +1,29 @@
+﻿using SubSonic.Core.Proxies;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace SubSonic.Core.Remoting
+{
+    public class RemotingProxyFactory
+        : ProxyFactory
+    {
+
+        protected override void BuildMethod(MethodInfo methodInfo, TypeBuilder typeBuilder)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void BuildProperty(PropertyInfo propertyInfo, TypeBuilder typeBuilder)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void GenerateILCodeForMethod(Type baseTypeOfProxy, MethodInfo methodInfo, ILGenerator mIL, Type[] inputArgTypes, Type returnType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
